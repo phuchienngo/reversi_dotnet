fdc0d23b391833bd680201cc78b1a625453012d3
Ngô Phú Chiến <57399555+phuchienngo@users.noreply.github.com>
2021-03-14 09:43:15 +0700

    update file

---
+++ client-console/Board.cs
@@ -0,0 +1,163 @@
+using System.Collections.Generic;
+using System.Linq;
+// ReSharper disable StringLiteralTypo
+// ReSharper disable IdentifierTypo
+// ReSharper disable InconsistentNaming
+
+namespace client_console
+{
+    public class Board
+    {
+        public readonly char[,] board;
+        public static readonly int[] weights = new[]
+        {
+            100, -1, 5, 2, 2, 5, -1, 100,
+            -1, -10,1, 1, 1, 1,-10, -1,
+            5 , 1,  1, 1, 1, 1,  1,  5, 
+            2 , 1,  1, 0, 0, 1,  1,  2,
+            2 , 1,  1, 0, 0, 1,  1,  2,
+            5 , 1,  1, 1, 1, 1,  1,  5,
+            -1,-10, 1, 1, 1, 1,-10, -1,
+            100, -1, 5, 2, 2, 5, -1, 100
+        };
+        public Board()
+        {
+            board = new char[8, 8];
+            for (var i = 0; i < 8; i++)
+            for (var j = 0; j < 8; j++)
+                board[i, j] = 'E';
+            board[3, 3] = board[4, 4] = 'W';
+            board[3, 4] = board[4, 3] = 'B';
+        }
+
+        public int GetRowID(char numericCharacter)
+        {
+            return numericCharacter - '1';
+        }
+
+        public int GetColumnID(char numericCharacter)
+        {
+            return numericCharacter - 'a';
+        }
+
+        public void SetValue((int, int) position, char value)
+        {
+            board[position.Item1, position.Item2] = value;
+        }
+
+        public char GetValue((int, int) position)
+        {
+            return board[position.Item1, position.Item2];
+        }
+
+        public char GetValue((char, char) position)
+        {
+            return board[GetRowID(position.Item2), GetColumnID(position.Item1)];
+        }
+
+        private bool IsOutOfRange(int r, int c)
+        {
+            return r < 0 || r > 7 || c < 0 || c > 7;
+        }
+
+        private bool IsDirectionPlaceable((char, char) position, (int, int) direction, char color)
+        {
+            if (GetValue(position) != 'E')
+                return false;
+            var rowId = GetRowID(position.Item2);
+            var columnId = GetColumnID(position.Item1);
+            var rowDirection = direction.Item1;
+            var columnDirection = direction.Item2;
+            for (var i = 1; i < 9; i++)
+            {
+                var r = rowId + i * rowDirection;
+                var c = columnId + i * columnDirection;
+                if (IsOutOfRange(r, c) || board[r, c] == 'E')
+                    return false;
+                if (board[r, c] == color)
+                    return i != 1;
+            }
+
+            return false;
+        }
+
+        public bool IsPlaceable((char, char) position, char color)
+        {
+            return IsDirectionPlaceable(position, (1, 0), color) ||
+                   IsDirectionPlaceable(position, (1, 1), color) ||
+                   IsDirectionPlaceable(position, (0, 1), color) ||
+                   IsDirectionPlaceable(position, (-1, 1), color) ||
+                   IsDirectionPlaceable(position, (-1, 0), color) ||
+                   IsDirectionPlaceable(position, (-1, -1), color) ||
+                   IsDirectionPlaceable(position, (0, -1), color) ||
+                   IsDirectionPlaceable(position, (1, -1), color);
+        }
+
+        public bool IsPlayable(char color)
+        {
+            return "12345678".Any(r => "abcdefgh".Any(c => IsPlaceable((c, r), color)));
+        }
+
+        public List<(char, char)> GetAllPossibleMoves(char color)
+        {
+            var result = new List<(char, char)>();
+            foreach (var r in "12345678")
+                result.AddRange(from c in "abcdefgh" where IsPlaceable((c, r), color) select (c, r));
+            return result;
+        }
+
+        private IEnumerable<(int, int)> GetDirectionFlips((char, char) position, (int, int) direction, char color)
+        {
+            if (GetValue(position) != 'E')
+                return new List<(int, int)>();
+            var result = new List<(int, int)>();
+            var rowId = GetRowID(position.Item2);
+            var columnId = GetColumnID(position.Item1);
+            for (var i = 1; i < 9; i++)
+            {
+                var r = rowId + i * direction.Item1;
+                var c = columnId + i * direction.Item2;
+                if (r < 0 || r > 7 || c < 0 || c > 7 || board[r, c] == 'E')
+                    return new List<(int, int)>();
+                if (board[r, c] == color)
+                    return result;
+                result.Add((r, c));
+            }
+
+            return null;
+        }
+
+        public List<(int, int)> GetFlips((char, char) position, char color)
+        {
+            var result = new List<(int, int)>();
+            result.AddRange(GetDirectionFlips(position, (1, 0), color));
+            result.AddRange(GetDirectionFlips(position, (1, 1), color));
+            result.AddRange(GetDirectionFlips(position, (0, 1), color));
+            result.AddRange(GetDirectionFlips(position, (-1, 1), color));
+            result.AddRange(GetDirectionFlips(position, (-1, 0), color));
+            result.AddRange(GetDirectionFlips(position, (-1, -1), color));
+            result.AddRange(GetDirectionFlips(position, (0, -1), color));
+            result.AddRange(GetDirectionFlips(position, (1, -1), color));
+            return result;
+        }
+
+        public void Update(string[] cellLines)
+        {
+            for (var r = 0; r < 8; r++)
+            {
+                var cells = cellLines[r].Split(' ');
+                for (var c = 0; c < 8; c++)
+                    board[r, c] = cells[c][0];
+            }
+        }
+
+        public int CountColor(char color)
+        {
+            var count = 0;
+            foreach (var item in board)
+                if (item == color)
+                    count++;
+            return count;
+        }
+    }
+}
---
+++ client-console/Program.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+
+// ReSharper disable IdentifierTypo
+// ReSharper disable UnusedMember.Local
+// ReSharper disable StringLiteralTypo
+// ReSharper disable InconsistentNaming
+
+namespace client_console
+{
+    internal static class Program
+    {
+        private const string ip = "209.97.169.233"; /*"localhost"*/
+        private const int port = 14003;
+        private static readonly Random rd = new();
+        private static List<string> history;
+        private static readonly List<string> openings = new();
+        private static bool usingOpeningList = true;
+
+        private static List<(char, char)> GetAvailableMoves(Board cell, char color)
+        {
+            var result = new List<(char, char)>();
+            foreach (var r in "12345678")
+                result.AddRange("abcdefgh".Where(c => cell.IsPlaceable((c, r), color)).Select(c => (c, r)));
+            return result;
+        }
+
+        private static void GetOpponentMove(Board cell)
+        {
+            var exceptList = new List<string> {"d4", "e4", "d5", "e5"};
+            foreach (var str in "12345678".SelectMany(r =>
+                from c in "abcdefgh"
+                let str = "" + c + r
+                where cell.GetValue((c, r)) != 'E' && !exceptList.Contains(str) && !history.Contains(str)
+                select str))
+                history.Add(str);
+        }
+
+        private static List<string> GetMoveFromOpening()
+        {
+            var strHistory = string.Join("", history);
+            var availableMoves = new List<string>();
+            var expireMoves = new List<string>();
+            foreach (var move in openings)
+            {
+                var str = move.ToLower();
+                if (str.Length <= strHistory.Length)
+                {
+                    expireMoves.Add(move);
+                    continue;
+                }
+
+                var isMatch = true;
+                var i = 0;
+                while (i < strHistory.Length && isMatch)
+                    if (str[i] != strHistory[i])
+                    {
+                        expireMoves.Add(move);
+                        isMatch = false;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+
+                if (isMatch)
+                    availableMoves.Add(str.Substring(i, 2));
+            }
+
+            foreach (var item in expireMoves) openings.Remove(item);
+
+            return availableMoves.Count > 0 ? availableMoves : null;
+        }
+
+        private static IEnumerable<((int, int), char)> DoMoves(Board cell, (char, char) position, char color)
+        {
+            var oldState = new List<((int, int), char)>();
+            var changedCells = cell.GetFlips(position, color);
+            foreach (var pos in changedCells)
+            {
+                oldState.Add((pos, cell.GetValue(pos)));
+                cell.SetValue(pos, color);
+            }
+
+            var r = cell.GetRowID(position.Item2);
+            var c = cell.GetColumnID(position.Item1);
+            oldState.Add(((r, c), cell.GetValue((r, c))));
+            cell.SetValue((r, c), color);
+            return oldState;
+        }
+
+        private static void UndoMoves(Board cell, IEnumerable<((int, int), char)> oldState)
+        {
+            foreach (var (valueTuple, item2) in oldState)
+                cell.SetValue(valueTuple, item2);
+        }
+
+        private static int Heuristic(Board cell, char color, string[] victoryCells)
+        {
+            var total = 0;
+
+            var oppColor = color == 'B' ? 'W' : 'B';
+            for (var i = 0; i < 64; i++)
+                if (cell.board[i / 8, i % 8] == color)
+                    total += Board.weights[i];
+                else if (cell.board[i / 8, i % 8] == oppColor)
+                    total -= Board.weights[i];
+
+            foreach (var item in victoryCells)
+                if (cell.GetValue((item[0], item[1])) == color)
+                    total += 10;
+            return total * 2 + 3 * (cell.CountColor(color) - cell.CountColor(oppColor));
+        }
+
+        private static int NegamaxHelper(Board cell, char color, int depth, int alpha, int beta, string[] victoryCells)
+        {
+            if (depth == 0)
+                return Heuristic(cell, color, victoryCells);
+            var moves = GetAvailableMoves(cell, color);
+            if (moves.Count == 0)
+            {
+                if (GetAvailableMoves(cell, color == 'W' ? 'B' : 'W').Count == 0)
+                    return Heuristic(cell, color, victoryCells);
+                var val = -NegamaxHelper(cell, color == 'W' ? 'B' : 'W', depth - 1, -beta, -alpha, victoryCells);
+                if (val >= beta)
+                    return val;
+                if (val > alpha)
+                    alpha = val;
+            }
+            else
+            {
+                foreach (var nextMove in moves)
+                {
+                    var oldState = DoMoves(cell, nextMove, color);
+                    var val = -NegamaxHelper(cell, color == 'W' ? 'B' : 'W', depth - 1, -beta, -alpha, victoryCells);
+                    UndoMoves(cell, oldState);
+                    if (val >= beta)
+                        return val;
+                    if (val > alpha)
+                        alpha = val;
+                }
+            }
+
+            return alpha;
+        }
+
+        private static string Negamax(Board cell, char color, int depth, string[] victoryCells)
+        {
+            var alpha = -65;
+            var beta = 65;
+            var move = string.Empty;
+            foreach (var nextMove in GetAvailableMoves(cell, color))
+            {
+                var oldState = DoMoves(cell, nextMove, color);
+                var val = -NegamaxHelper(cell, color == 'W' ? 'B' : 'W', depth - 1, -beta, -alpha, victoryCells);
+                UndoMoves(cell, oldState);
+                if (val >= beta)
+                    return "" + nextMove.Item1 + nextMove.Item2;
+                if (val > alpha)
+                {
+                    alpha = val;
+                    move = "" + nextMove.Item1 + nextMove.Item2;
+                }
+            }
+
+            return move;
+        }
+
+        private static int NegaScoutHelper(Board cell, char color, int depth, int alpha, int beta,
+            string[] victoryCells)
+        {
+            if (depth == 0)
+                return Heuristic(cell, color, victoryCells);
+            var bestScore = int.MinValue;
+            var adaptiveBeta = beta;
+            var allPossibleMoves = cell.GetAllPossibleMoves(color);
+            if (allPossibleMoves.Count == 0)
+                return Heuristic(cell, color, victoryCells);
+            foreach (var nextMove in allPossibleMoves)
+            {
+                var oldStates = DoMoves(cell, nextMove, color);
+                var currentScore = -NegaScoutHelper(cell, color == 'B' ? 'W' : 'B', depth - 1, -adaptiveBeta,
+                    -Math.Max(alpha, bestScore), victoryCells);
+                if (currentScore > bestScore)
+                {
+                    if (adaptiveBeta == beta || depth < 3)
+                        bestScore = currentScore;
+                    else
+                        bestScore = -NegaScoutHelper(cell, color == 'B' ? 'W' : 'B', depth - 1, -beta, -currentScore,
+                            victoryCells);
+
+                    if (bestScore >= beta)
+                    {
+                        UndoMoves(cell, oldStates);
+                        return Heuristic(cell, color, victoryCells);
+                    }
+                }
+
+                UndoMoves(cell, oldStates);
+                adaptiveBeta = Math.Max(alpha, bestScore) + 1;
+            }
+
+            return bestScore;
+        }
+
+        private static string NegaScout(Board cell, char color, int depth, string[] victoryCells)
+        {
+            var alpha = int.MinValue;
+            var beta = int.MaxValue;
+            var bestScore = int.MinValue;
+            var bestMove = "NULL";
+            var adaptiveBeta = beta;
+            var nextMoves = cell.GetAllPossibleMoves(color);
+            if (nextMoves.Count == 0)
+                return "NULL";
+            foreach (var move in nextMoves)
+            {
+                var oldStates = DoMoves(cell, move, color);
+                var currentScore = -NegaScoutHelper(cell, color == 'B' ? 'W' : 'B', depth - 1, -adaptiveBeta,
+                    -Math.Max(alpha, bestScore), victoryCells);
+                if (currentScore > bestScore)
+                {
+                    if (adaptiveBeta == beta || depth < 3)
+                    {
+                        bestScore = currentScore;
+                        bestMove = "" + move.Item1 + move.Item2;
+                    }
+                    else
+                    {
+                        bestScore = -NegaScoutHelper(cell, color == 'B' ? 'W' : 'B', depth - 1, -beta, -currentScore,
+                            victoryCells);
+                        bestMove = "" + move.Item1 + move.Item2;
+                    }
+
+                    if (bestScore >= beta)
+                    {
+                        UndoMoves(cell, oldStates);
+                        return bestMove;
+                    }
+                }
+
+                UndoMoves(cell, oldStates);
+                adaptiveBeta = Math.Max(alpha, bestScore) + 1;
+            }
+
+            return bestMove;
+        }
+
+        private static string Bot(string[] victoryCell, Board cell, string you)
+        {
+            var color = you == "BLACK" ? 'B' : 'W';
+            if (usingOpeningList)
+            {
+                GetOpponentMove(cell);
+                var availableMoves = GetMoveFromOpening();
+                if (availableMoves != null)
+                    return availableMoves[rd.Next(0, availableMoves.Count - 1)];
+                usingOpeningList = false;
+                openings.Clear();
+            }
+
+            return NegaScout(cell, color, 8, victoryCell);
+        }
+
+        private static string CallBot(string gameInfo)
+        {
+            var lines = gameInfo.Split('\n');
+            var victoryCell = lines[1].Split(' ');
+            var cell = new Board();
+            cell.Update(new List<string>(lines).GetRange(3, 8).ToArray());
+            var you = lines[12];
+            var result = Bot(victoryCell, cell, you);
+            if (result != "NULL")
+            {
+                history.Add(result);
+                return result;
+            }
+
+            return "NULL";
+        }
+
+        private static void Main()
+        {
+            var tcpClient = new TcpClient();
+            if (!tcpClient.ConnectAsync(ip, port).Wait(3000))
+                throw new InvalidProgramException("Cannot connect to server");
+            Console.WriteLine("Connected to server");
+            var stream = tcpClient.GetStream();
+            history = new List<string>();
+            try
+            {
+                var streamReader = new StreamReader("opening.txt");
+                while (!streamReader.EndOfStream) openings.Add(streamReader.ReadLine());
+                streamReader.Dispose();
+                streamReader.Close();
+            }
+            catch
+            {
+                usingOpeningList = false;
+            }
+
+            while (true)
+            {
+                var data = new byte[256];
+                var bytes = stream.Read(data, 0, data.Length);
+                var response = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine(response);
+                if (!Regex.IsMatch(response, "^victory_cell"))
+                    break;
+                var result = CallBot(response);
+                data = Encoding.ASCII.GetBytes(result);
+                stream.Write(data, 0, data.Length);
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
